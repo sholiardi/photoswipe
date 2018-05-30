919e29acd4b593ad52fa33b49863d0bf6ac099f2
Crocho de ICT <crochodeict@gmail.com>
2018-05-30 09:57:15 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <link rel="stylesheet" href="./dist/photoswipe.css" />
+    <link rel="stylesheet" href="./dist/default-skin/default-skin.css" />
+    <link rel="stylesheet" href="./style.css" />
+    <script src="./dist/photoswipe.min.js"></script>
+    <script src="./dist/photoswipe-ui-default.min.js"></script>
+  </head>
+  <body>
+    <?php include 'photoswipe.php'; ?>
+    <script src="options.js"></script>
+  </body>
+</html>
